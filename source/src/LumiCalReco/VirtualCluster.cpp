75ebad456523a4a11634e677079671edc1462556
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-07-28 09:18:14 +0000

    Adapt VirtualCluster functions

--- source/src/LumiCalReco/VirtualCluster.cpp
+++ source/src/LumiCalReco/VirtualCluster.cpp
@@ -4,29 +4,29 @@
 #include <iomanip>
 
 
-VirtualCluster::VirtualCluster():
-  _x(0.0),
-  _y(0.0),
-  _z(0.0)
-{}
+VirtualCluster::VirtualCluster()
+{
+  _position[0] = 0.0;
+  _position[1] = 0.0;
+  _position[2] = 0.0;
+}
 
-VirtualCluster::VirtualCluster(double x, double y, double z):
-  _x(x),
-  _y(y),
-  _z(z)
-{}
+VirtualCluster::VirtualCluster(double x, double y, double z)
+{
+  _position[0] = x;
+  _position[1] = y;
+  _position[2] = z;
+}
 
 void VirtualCluster::clear() {
-
-  _x = 0.0;
-  _y = 0.0;
-  _z = 0.0;
-
+  _position[0] = 0.0;
+  _position[1] = 0.0;
+  _position[2] = 0.0;
 }
 
 std::ostream& operator<<(std::ostream & o, const VirtualCluster& rhs) {
-  o << std::setw(13) << rhs._x
-    << std::setw(13) << rhs._y
-    << std::setw(13) << rhs._z;
+  o << std::setw(13) << rhs._position[0]
+    << std::setw(13) << rhs._position[1]
+    << std::setw(13) << rhs._position[2];
   return o;
 }
