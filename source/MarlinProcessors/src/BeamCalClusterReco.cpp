a1f5de9a6cd711eea9817facc1530c6fbe7dc0b2
Strahinja Lukic <slukic@vinca.rs>
2014-08-08 11:17:52 +0000

    Minor temporary changes

--- source/MarlinProcessors/src/BeamCalClusterReco.cpp
+++ source/MarlinProcessors/src/BeamCalClusterReco.cpp
@@ -537,6 +537,7 @@ void BeamCalClusterReco::processEvent( LCEvent * evt ) {
     particle->setMomentum ( momentumCluster ) ;
     particle->setEnergy ( energyCluster ) ;
     particle->addCluster( cluster ) ;
+    particle->setGoodnessOfPID((*it)->getCorrelEMShower());
     if((*it)->getCorrelEMShower() > m_EMcorrelThreshold) particle->setType(0); // EM type
     else particle->setType(1); // hadronic type
 
@@ -690,10 +691,11 @@ std::vector<BCRecoObject*> BeamCalClusterReco::FindClusters(const BCPadEnergies&
   //////////////////////////////////////////
   // This calls the clustering function!
   //////////////////////////////////////////
-  const std::vector<BeamCalCluster> &bccs =
-    signalPads.lookForNeighbouringClustersOverWithVetoAndCheck(backgroundPads, backgroundSigma, *m_bcpCuts);
+//  const std::vector<BeamCalCluster> &bccs = signalPads.lookForNeighbouringClustersOverWithVetoAndCheck(backgroundPads, backgroundSigma, *m_bcpCuts);
+  std::vector<BeamCalCluster> bccs;
+  bccs.push_back(signalPads.lookForNeighbouringClustersOverWithVeto(backgroundPads,  *m_bcpCuts));
 
-  int iCluster=0; // SL:DELETE
+//  int iCluster=0; // SL:DELETE
   for (std::vector<BeamCalCluster>::const_iterator it = bccs.begin(); it != bccs.end(); ++it) {
 
     streamlog_out(MESSAGE2) << title;
@@ -713,14 +715,14 @@ std::vector<BCRecoObject*> BeamCalClusterReco::FindClusters(const BCPadEnergies&
 //      bcp.subtractEnergies(backgroundPads); (Subtracting from zeros, as well?)
       TH3D *bch3d = MakeBeamCalHisto(&bcp);
       TH1D *layers = (TH1D*)bch3d->Project3D("x");
-      TH1D *radial = (TH1D*)bch3d->Project3D("y"); // SL:DELETE
+/*      TH1D *radial = (TH1D*)bch3d->Project3D("y"); // SL:DELETE
       TCanvas c("clusterplot", "Cluster Plot", 800, 1200); // SL:DELETE
       c.Divide(1,2,.01,.01); // SL:DELETE
       c.cd(1); layers->Draw(); // SL:DELETE
       c.cd(2); radial->Draw(); // SL:DELETE
       iCluster++; // SL:DELETE
       c.Print(Form("plots%i-%i.pdf", m_nEvt, iCluster)); // SL:DELETE
-
+*/
       float xStart=0, correlEMShower=0;
       // Test correlation with the standard EM shower profile and determine the shower starting position xStart
       Test(layers, xStart, correlEMShower);
