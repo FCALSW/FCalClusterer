72306a7e266526fb917a4246fce26e7e1dfb4cb2
Strahinja Lukic <slukic@vinca.rs>
2014-08-29 09:51:20 +0000

    Adding ReadBC_SepEvt processor

---
+++ source/MarlinProcessors/include/ReadBC_SepEvt.hh
@@ -0,0 +1,81 @@
+#ifndef ReadBeamCal_h
+#define ReadBeamCal_h 1
+
+#include <BCPadEnergies.hh>
+
+#include <string>
+#include <vector>
+
+#include <marlin/Processor.h>
+#include <lcio.h>
+
+
+class TH1D;
+class TH2D;
+class TH3D;
+class TFile;
+class TRandom3;
+class TTree;
+
+
+class ReadBC_SepEvt : public marlin::Processor {
+  
+ public:
+  
+  virtual Processor*  newProcessor() { return new ReadBC_SepEvt ; }
+  
+  
+  ReadBC_SepEvt() ;
+  ~ReadBC_SepEvt() {};
+
+  /** Called at the begin of the job before anything is read.
+   * Use to initialize the processor, e.g. book histograms.
+   */
+  virtual void init() ;
+  
+  /** Called for every run.
+   */
+  virtual void processRunHeader( LCRunHeader* run ) ;
+  
+  /** Called for every event - the working horse.
+   */
+  virtual void processEvent( LCEvent * evt ) ; 
+  
+  
+  virtual void check( LCEvent * evt ) ; 
+  
+  
+  /** Called after data processing for clean up.
+   */
+  virtual void end() ;
+  
+  
+ protected:
+
+  /** Input collection name.
+   */
+  std::string m_colNameBCal, m_nameOutputFile, m_nameFinalOutputFile, m_nameInputFile;
+  //std::string m_pdftitle;
+  int m_nRun ;
+  int m_nEvt ;
+  double m_probFactor;
+
+  TRandom3 *m_random3;
+
+  BCPadEnergies* m_padEnergiesLeft;
+  BCPadEnergies* m_padEnergiesRight;
+  TTree *tree;
+
+  BeamCalGeo* m_bcg;
+
+private://to shut the warnings up
+  ReadBC_SepEvt(const ReadBC_SepEvt&);
+  ReadBC_SepEvt& operator=(const ReadBC_SepEvt&);
+
+} ;
+
+
+#endif
+
+
+
