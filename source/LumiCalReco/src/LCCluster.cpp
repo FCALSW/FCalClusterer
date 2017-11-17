deb47234e8e8b9e59f6e047cdcf6dbef0bf12ba4
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-11-17 11:31:15 +0100

    LumiCalReco: LCCluster add recalculatePositionFromHits function
    calculate cluster position from the associated hits

--- source/LumiCalReco/src/LCCluster.cpp
+++ source/LumiCalReco/src/LCCluster.cpp
@@ -58,3 +58,51 @@ std::ostream& operator<<(std::ostream & o, const LCCluster& rhs) {
     << "  pos(theta,phi) =  ( " << std::setw(10) << rhs._theta << " , " << std::setw(10) << rhs._phi << " )";
   return o;
 }
+
+/** recalculate the position of the cluster based on the theta and phi averages
+ *
+ * Resolution in Theta (R) is better than in RPhi so averaging theta gives better results
+ */
+void LCCluster::recalculatePositionFromHits(GlobalMethodsClass const& gmc) {
+  const double logConstant(gmc.GlobalParamD.at(GlobalMethodsClass::LogWeightConstant));
+
+  //re-set new cluster energy
+  _energy = 0.0;
+  for (auto const& calHit : _caloHits) {
+    _energy += calHit->getEnergy();
+  }
+
+  double thetaTemp(0.0), weightsTemp(0.0), xTemp(0.0), yTemp(0.0), zTemp(0.0);
+  for (auto const& calHit : _caloHits) {
+    const auto*  pos    = calHit->getPosition();
+    const double weight = GlobalMethodsClass::posWeight(calHit->getEnergy(), _energy, _method, logConstant);
+    if (not(weight > 0))
+      continue;
+
+    const double r     = sqrt(pos[0] * pos[0] + pos[1] * pos[1]);
+    const double theta = atan(r / pos[2]);
+
+    thetaTemp += theta * weight;
+
+    xTemp += pos[0] * weight;
+    yTemp += pos[1] * weight;
+    zTemp += pos[2] * weight;
+
+    weightsTemp += weight;
+  }
+
+  xTemp /= weightsTemp;
+  yTemp /= weightsTemp;
+  zTemp /= weightsTemp;
+
+  thetaTemp /= weightsTemp;
+
+  _phi   = atan2(yTemp, xTemp);
+  _theta = thetaTemp;
+
+  const double r = sqrt(xTemp * xTemp + yTemp * yTemp + zTemp * zTemp);
+
+  _position[0] = r * sin(_theta) * cos(_phi);
+  _position[1] = r * sin(_theta) * sin(_phi);
+  _position[2] = r * cos(_theta);
+}
